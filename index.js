2e92f2484050e4ef0774e6d6a4e16571ab025331
Kyubin Han <kyubinhan11@gmail.com>
2021-11-14 15:56:52 -0800

    Allow users to download images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Inspired by https://dev.to/microworlds/image-downloader-with-puppeteer-and-the-fetch-api-5b8e
 
-const fs = require("fs").promises;
+const fetch = require("node-fetch");
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 const path = require("path");
 
@@ -16,7 +17,7 @@ async function extractImageLinks(pageURL) {
     // Wait for the html body tag to render
     await page.waitForSelector("body");
 
-    const urls = await page.evaluate(() => {
+    const imageBank = await page.evaluate(() => {
       // Select the container of the thumbnail images
       const altImagesContainer = document.querySelector("#altImages");
       // Then select individual images and convert them into an array
@@ -24,10 +25,25 @@ async function extractImageLinks(pageURL) {
         altImagesContainer.querySelectorAll("li.imageThumbnail img")
       );
 
-      return imageTags.map((image) => image.src);
+      const imageArray = [];
+
+      imageTags.map((image) => {
+        const src = image.src;
+
+        const srcArray = src.split("/");
+        const pos = srcArray.length - 1;
+        const filename = srcArray[pos];
+
+        imageArray.push({
+          src,
+          filename,
+        });
+      });
+
+      return imageArray;
     });
 
-    return urls;
+    return imageBank;
   } catch (error) {
     throw error;
   } finally {
@@ -35,23 +51,26 @@ async function extractImageLinks(pageURL) {
   }
 }
 
-async function writeToCSV(pageURL, imageURLs, customDownloadPath) {
-  const header = "Product,Images";
-  const data = imageURLs.map((url, index) => {
-    if (index === 0) {
-      return `"${pageURL}","${url}"`;
-    }
+function checkDirectory(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {
+      recursive: true,
+    });
+  }
+}
 
-    return `,"${url}"`;
-  });
-  const rows = [header, ...data];
+function getDirectory() {
+  const directory = process.argv[3] || `${path.dirname(__filename)}/images`;
+  checkDirectory(directory);
 
-  const downloadPath =
-    customDownloadPath || `${path.dirname(__filename)}/data.csv`;
+  return directory;
+}
 
-  await fs.writeFile(downloadPath, rows.join("\n"));
-  console.log("Image URLs have been stored in CSV file successfully!");
-  console.log(`Path: ${downloadPath}`);
+async function saveImageToDisk(url, filename, directory) {
+  const res = await fetch(url);
+  const dest = fs.createWriteStream(`${directory}/${filename}`);
+
+  res.body.pipe(dest);
 }
 
 (async function () {
@@ -64,10 +83,19 @@ async function writeToCSV(pageURL, imageURLs, customDownloadPath) {
 
     console.log("Running the script...");
 
-    const imageURLs = await extractImageLinks(pageURL);
+    const result = await extractImageLinks(pageURL);
 
-    const customDownloadPath = process.argv[3];
-    await writeToCSV(pageURL, imageURLs, customDownloadPath);
+    const directory = getDirectory();
+
+    // Download all the images in parallel
+    await Promise.all(
+      result.map(({ src, filename }) =>
+        saveImageToDisk(src, filename, directory)
+      )
+    );
+
+    console.log("Images have been downloaded successfully!");
+    console.log(`Check directory: ${directory}`);
   } catch (error) {
     console.log(error.message);
   }
